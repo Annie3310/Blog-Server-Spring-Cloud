adb250ed233d6fd2f7bbdd49fdb0825a226ce33f
王金义 <jinyi.wang@liangyihui.net>
2022-01-06 17:23:06 +0800

    添加 Redis 配置

--- controller/src/main/java/top/cattycat/controller/config/SpringConfig.java
+++ controller/src/main/java/top/cattycat/controller/config/SpringConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("spring")
 public class SpringConfig {
     private Datasource datasource;
+    private Redis redis;
 
     public Datasource getDatasource() {
         return datasource;
@@ -16,6 +17,14 @@ public class SpringConfig {
 
     public void setDatasource(Datasource datasource) {
         this.datasource = datasource;
+    }
+
+    public Redis getRedis() {
+        return redis;
+    }
+
+    public void setRedis(Redis redis) {
+        this.redis = redis;
     }
 
     public static class Datasource {
@@ -41,6 +50,35 @@ public class SpringConfig {
 
         public void setUsername(String username) {
             this.username = username;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+    public static class Redis {
+        private String host;
+        private String port;
+        private String password;
+
+        public String getHost() {
+            return host;
+        }
+
+        public void setHost(String host) {
+            this.host = host;
+        }
+
+        public String getPort() {
+            return port;
+        }
+
+        public void setPort(String port) {
+            this.port = port;
+        }
+
+        public String getPassword() {
+            return password;
         }
 
         public void setPassword(String password) {
