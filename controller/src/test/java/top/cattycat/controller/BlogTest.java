b5bc6eaa6118813f2a2d859742438c19f46df82c
王金义 <jinyi.wang@liangyihui.net>
2022-01-06 17:21:28 +0800

    添加测试用例

---
+++ controller/src/test/java/top/cattycat/controller/BlogTest.java
@@ -0,0 +1,13 @@
+package top.cattycat.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author 王金义
+ * @date 2022/1/6
+ */
+@SpringBootTest
+public class BlogTest {
+
+}
