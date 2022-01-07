2e036bbbbf153fdb38f68b0b59b2016cda714013
王金义 <jinyi.wang@liangyihui.net>
2022-01-07 18:32:07 +0800

    添加登录接口

---
+++ controller/src/main/java/top/cattycat/controller/controller/Login.java
@@ -0,0 +1,13 @@
+package top.cattycat.controller.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 王金义
+ * @date 2022/1/7
+ */
+@RestController
+public class Login {
+
+}
