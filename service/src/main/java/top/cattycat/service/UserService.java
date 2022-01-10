181d2c1d055b4bb1d8dca165ff851d885852938b
王金义 <jinyi.wang@liangyihui.net>
2022-01-10 10:48:14 +0800

    添加 OAuth 用户类

---
+++ service/src/main/java/top/cattycat/service/UserService.java
@@ -0,0 +1,11 @@
+package top.cattycat.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import top.cattycat.common.pojo.entity.User;
+
+/**
+ * @author 王金义
+ * @date 2022/1/10
+ */
+public interface UserService extends IService<User> {
+}
---
+++ service/src/main/java/top/cattycat/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package top.cattycat.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import top.cattycat.common.pojo.entity.User;
+import top.cattycat.mapper.mapper.UserMapper;
+
+/**
+ * @author 王金义
+ * @date 2022/1/10
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> {
+
+}
