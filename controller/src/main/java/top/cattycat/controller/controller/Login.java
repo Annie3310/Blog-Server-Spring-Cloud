9fe87f05aa1219f51e1f1fa1389d74454b1c19a4
王金义 <jinyi.wang@liangyihui.net>
2022-01-12 17:50:52 +0800

    Complete login feature

--- controller/src/main/java/top/cattycat/controller/controller/Login.java
+++ controller/src/main/java/top/cattycat/controller/controller/Login.java
@@ -1,17 +1,22 @@
 package top.cattycat.controller.controller;
 
+import io.jsonwebtoken.Claims;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.*;
 import top.cattycat.common.enums.ResponseEnum;
+import top.cattycat.common.exception.BlogException;
+import top.cattycat.common.pojo.oauth.github.request.ValidateRequest;
 import top.cattycat.common.pojo.oauth.github.response.GitHubAuthorizationResponse;
 import top.cattycat.common.pojo.oauth.github.response.GitHubGetAccessTokenResponse;
 import top.cattycat.common.pojo.oauth.github.response.GitHubUserInfoResponse;
+import top.cattycat.common.pojo.oauth.github.response.LoggedInResponse;
 import top.cattycat.common.pojo.response.ResponseFactory;
 import top.cattycat.common.pojo.response.ResponseResult;
+import top.cattycat.common.pojo.vo.UserVO;
 import top.cattycat.common.util.JwtUtils;
+import top.cattycat.controller.constant.Constant;
 import top.cattycat.service.UserService;
 
 import java.util.Objects;
@@ -25,65 +30,65 @@ import java.util.concurrent.TimeUnit;
 @RequestMapping("login/oauth/github")
 public class Login {
     private final UserService userService;
-    private final RedisTemplate stringRedisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
-    private final static String USER_ACCESS_TOKEN_TEMPLATE = "user-access-token-%s";
-    private final static String USER_JWT_TOKEN_TEMPLATE = "user-jwt-token-%s";
-
-    public Login(UserService userService, RedisTemplate redisTemplate) {
+    public Login(UserService userService, RedisTemplate<String, String> redisTemplate) {
         this.userService = userService;
-        this.stringRedisTemplate = redisTemplate;
+        this.redisTemplate = redisTemplate;
     }
 
     @GetMapping("/authorize")
-    public ResponseResult<String> authorize(GitHubAuthorizationResponse response) {
+    public void authorize(GitHubAuthorizationResponse response) {
         final String state = response.getState();
         final GitHubGetAccessTokenResponse accessTokenResponse = this.userService.getAccessToken(response);
         final String accessToken = accessTokenResponse.getAccessToken();
         final GitHubUserInfoResponse userInformation = this.userService.getUserInformation(accessToken);
         final Long id = userInformation.getId();
-        final ResponseFactory<String> resultFactory = new ResponseFactory<>();
 
         if (Objects.isNull(id)) {
-            return resultFactory.error(ResponseEnum.CONNECT_TO_GITHUB_FAILED);
+            throw new BlogException(ResponseEnum.CONNECT_TO_GITHUB_FAILED);
         }
         // Whether user has registered
         final String jwtToken = JwtUtils.getToken(userInformation);
         if (this.userService.isRegistered(id)) {
             this.userService.updateLastLoginTime(id);
             this.setAccessTokenInRedis(userInformation, accessToken, state, jwtToken);
-            return resultFactory.success(jwtToken);
         } else {
             // It will register if user has not registered
             if (this.userService.register(userInformation)) {
                 this.setAccessTokenInRedis(userInformation, accessToken, state, jwtToken);
-                return resultFactory.success(jwtToken);
-            } else {
-                return resultFactory.error(ResponseEnum.REGISTER_FAILED);
             }
         }
     }
 
     @PostMapping("/validate")
-    public ResponseResult<String> validate(@RequestBody String state) {
-        final ValueOperations<String, String> opsForValue = this.stringRedisTemplate.opsForValue();
-        String jwtToken = opsForValue.get(state);
-        final ResponseFactory<String> resultFactory = new ResponseFactory<>();
+    public ResponseResult<LoggedInResponse> validate(@RequestBody ValidateRequest state) {
+        final ValueOperations<String, String> opsForValue = this.redisTemplate.opsForValue();
+        final String jwtTokenKey = String.format(Constant.USER_JWT_TOKEN_TEMPLATE, state.getState());
+        final String jwtToken = opsForValue.get(jwtTokenKey);
+        final ResponseFactory<LoggedInResponse> resultFactory = new ResponseFactory<>();
         if (StringUtils.isEmpty(jwtToken)) {
             return resultFactory.error(ResponseEnum.LOGIN_IS_NOT_COMPLETE);
         } else {
-            return resultFactory.success(jwtToken);
+            final Claims claims = JwtUtils.parseToken(jwtToken);
+            final String login = String.valueOf(claims.get("login"));
+            final String avatar = String.valueOf(claims.get("avatar"));
+            final UserVO userVO = new UserVO().setLogin(login).setAvatarUrl(avatar);
+            LoggedInResponse result = new LoggedInResponse().setAccessToken(avatar).setUserInfo(userVO);
+            this.redisTemplate.delete(jwtTokenKey);
+            return resultFactory.success(result);
         }
     }
     /**
      * Set user's access token and jwt token into redis
-     * @param userInformation
-     * @param accessToken
+     * @param userInformation User information
+     * @param accessToken access token
      */
     private void setAccessTokenInRedis(GitHubUserInfoResponse userInformation, String accessToken, String state, String jwtToken) {
-        final String key = String.format(USER_ACCESS_TOKEN_TEMPLATE, userInformation.getId());
-        final ValueOperations<String, String> opsForValue = this.stringRedisTemplate.opsForValue();
-        opsForValue.set(key, accessToken, 7, TimeUnit.DAYS);
-        opsForValue.set(state, jwtToken);
+        final String accessTokenKey = String.format(Constant.USER_ACCESS_TOKEN_TEMPLATE, userInformation.getId());
+        final String jwtTokenKey = String.format(Constant.USER_JWT_TOKEN_TEMPLATE, state);
+        final ValueOperations<String, String> opsForValue = this.redisTemplate.opsForValue();
+        opsForValue.set(accessTokenKey, accessToken, 7, TimeUnit.DAYS);
+        opsForValue.set(jwtTokenKey, jwtToken);
     }
 }
