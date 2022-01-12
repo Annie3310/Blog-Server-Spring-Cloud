cbd70c30d59821d7bc352e30dbd4f25da14e61ca
王金义 <jinyi.wang@liangyihui.net>
2022-01-12 17:49:26 +0800

    抽出公共常量

---
+++ controller/src/main/java/top/cattycat/controller/constant/Constant.java
@@ -0,0 +1,10 @@
+package top.cattycat.controller.constant;
+
+/**
+ * @author 王金义
+ * @date 2022/1/12
+ */
+public class Constant {
+    public final static String USER_ACCESS_TOKEN_TEMPLATE = "user-access-token-%s";
+    public final static String USER_JWT_TOKEN_TEMPLATE = "user-jwt-token-%s";
+}
